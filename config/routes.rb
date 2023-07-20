d01fe41a16e1c5c8dbd912f513fdbf3613d0bfb4
hoangnguyen249 <Joshbub2409@gmail.com>
2023-07-19 22:40:23 -0500

    use devise to create user and add product table

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
+  devise_scope :user do
+    get '/users', to: 'devise/registrations#new'
+    get '/users/password', to: 'devise/passwords#new'
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+  get 'pages/home'
+  get 'pages/about'
+  root "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
