a40f362781dc3986ea0cfe82d68445632b301c66
hoangnguyen249 <Joshbub2409@gmail.com>
2023-07-20 09:58:14 -0500

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  get 'admin/index'
   devise_for :users
   devise_scope :user do
     get '/users', to: 'devise/registrations#new'
