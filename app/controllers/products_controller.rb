5e0b9ba2db659e843a1a5b612ab7821562a80f55
hoangnguyen249 <Joshbub2409@gmail.com>
2023-07-28 21:45:08 -0500

    add paginated

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,7 +3,7 @@ class ProductsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
-    @products = Product.all
+    @products = Product.all.page(params[:page]).per(20)
   end
 
   def show
