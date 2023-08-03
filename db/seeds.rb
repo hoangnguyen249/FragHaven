f9734c3d8879e3b9b42d30c076ed721dc92067a1
hoangnguyen249 <Joshbub2409@gmail.com>
2023-08-03 06:37:28 -0500

    change seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,20 +32,17 @@ products.each do |product|
   product.categories << random_category
 end
 require 'httparty'
-
+products = Product.where(id: 30..60)
 def fetch_random_image_url
-  response = HTTParty.get('https://picsum.photos/seed/picsum/500/500')
+  random_id = rand(10..50)
+  response = HTTParty.get("https://picsum.photos/id/#{random_id}/200/300")
   return response.request.last_uri.to_s
 end
 
-
-products_without_images = Product.where.not(id: Product.joins(:images_attachments))
-
-
-products_without_images.each do |product|
+products.each do |product|
   random_image_url = fetch_random_image_url
   product.images.attach(io: URI.open(random_image_url), filename: 'random_image.jpg')
 end
-products_without_images.each(&:save)
+products.each(&:save)
 =end
 
