42f7bc325d4171711d641aa1cdef1f05e850ef89
hoangnguyen249 <Joshbub2409@gmail.com>
2023-08-02 22:22:47 -0500

    finish admin dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -7,14 +7,15 @@ Rails.application.routes.draw do
   end
   get 'pages/home'
   get 'pages/about'
+  resources :categories
   resources :products do
     collection do
       get 'search'
     end
   end
   root "pages#home"
-
-  resources :categories, only: [:index, :show]
+  get 'admin/index'
+  resources :categories
 
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
