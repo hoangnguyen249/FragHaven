8207cd24170328199274d1ff63f25cd73dad93f4
hoangnguyen249 <Joshbub2409@gmail.com>
2023-08-03 00:25:30 -0500

    add

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_20_172504) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_03_052235) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -62,6 +62,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_20_172504) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id", null: false
+    t.boolean "on_sale"
+    t.decimal "sale_price"
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
